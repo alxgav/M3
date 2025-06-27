1ab2b805f9f0f43d5792c23daf6b4f1491d926ab
alxgav <alxgav@gmail.com>
2025-06-27 12:45:17 +0200

    update model

--- app/core/models.py
+++ app/core/models.py
@@ -10,9 +10,26 @@ class Base(AsyncAttrs, DeclarativeBase):
 
 
 class Material(Base, TimestampMixin):
-    __tablename__ = 'materials'  # Explicitly set the table name
+    __tablename__ = 'materials'  
     name_material: Mapped[str] = Column(String, nullable=False)
     time_material: Mapped[float] = Column(Float, nullable=False)
     note_material: Mapped[str] = Column(String, nullable=True)
 
+class Workout(Base, TimestampMixin):
+    __tablename__ = 'workouts'  
+    shift_work: Mapped[str] = Column(String, nullable=False)
+    time_workout: Mapped[float] = Column(Float, nullable=False)
+    user_id: Mapped[str] = mapped_column(String, nullable=False)
 
+class WorkoutMaterial(Base, TimestampMixin):
+    __tablename__ = 'workout_materials'  
+    workout_id: Mapped[str] = mapped_column(String, nullable=False)
+    material_id: Mapped[str] = mapped_column(String, nullable=False)
+    time_workout_material: Mapped[float] = Column(Float, nullable=False)
+    note_workout_material: Mapped[str] = Column(String, nullable=True)
+
+class User(Base, TimestampMixin):
+    __tablename__ = 'users'  
+    username: Mapped[str] = Column(String, unique=True, nullable=False)
+    password: Mapped[str] = Column(String, nullable=False)
+    is_active: Mapped[bool] = Column(String, default=True, nullable=False)
